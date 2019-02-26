d050a70e3694582092834431b4013e655d52a4ba
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-02-26 18:20:46 +0100

    add axios and jquery

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -14,8 +14,68 @@ const StyledContainer = styled.div`
   }
 `;
 
-export default function Container() {
-  return (
-    <StyledContainer>Http Ajax</StyledContainer>
-  );
+export default class Container extends React.Component {
+  state = {
+    error: null,
+    loading: false,
+    people: [],
+  }
+
+  setPeople = people => {
+    this.setState({
+      loading: false,
+      people,
+    });
+  }
+
+  setError = error => {
+    this.setState({
+      loading: false,
+      error: error.message,
+    });
+  }
+
+  doRequest = () => {
+    this.setState({ loading: true });
+    fetch('http://demo6368739.mockable.io/')
+      .then(res => res.json())
+      .then(this.setPeople)
+      .catch(this.setError);
+  }
+
+  componentDidMount() {
+    this.doRequest();
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <StyledContainer>Loading...</StyledContainer>;
+    }
+
+    if (this.state.error) {
+      return (
+        <StyledContainer>
+          This is horrible! {this.state.error}
+          <br />
+          <button onClick={this.doRequest}>Retry</button>
+        </StyledContainer>
+      );
+    }
+
+    return (
+      <StyledContainer>
+        {
+          this.state.people.length > 0 &&
+          this.state.people.map(person => (
+            <div>
+              name: {person.name}
+              <br />
+              age: {person.age}
+            </div>
+          ))
+        }
+        <button onClick={this.doRequest}>Fetch again</button>
+      </StyledContainer>
+    );
+  }
 }
