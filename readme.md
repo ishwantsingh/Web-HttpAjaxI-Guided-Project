00da179cd71552c34ccedd51a4bc99e512a142ca
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-02-27 09:38:55 +0100

    wip

--- readme.md
+++ readme.md
@@ -9,4 +9,19 @@
 We need `node` & `npm`, as well as packages `live-server` and `eslint` installed _globally_. VSCode's `eslint` extension is recommended. Command line commands are run inside the project folder. This guide uses npm but should work the same using yarn.
 
 # Objectives
-  * Learn about HTTP and AJAX
+  * Learn about HTTP and AJAX.
+  * Learn to use the Network tab in Chrome Devtools.
+  * Learn to do network GET requests from our JavaScript code.
+
+# Steps
+  * Adjust Chrome Devtools settings to make the most of the Network tab.
+  * Load a website and break down the `Network` tab.
+  * Set up a mock endpoint using `Mockable.io` that serves a person with an id, name and age.
+  * Explain `Postman` interface and make a request to our fake endpoint to prove it works.
+  * Set up a component to hold `error`, `loading` and `person` slices of state.
+  * Build `setPerson`, `setError` methods.
+  * Build a `fetchPerson` method that uses `fetch` to hit our endpoint. Focus on happy path.
+  * Create UI to display the different states of `loading`, `error` and `person`.
+  * Improve our methods to adjust `loading` and `error` states. Non-happy path.
+  * Demonstrate using `jQuery` and `axios`.
+  * Explain how to build a function that returns a Promise.
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import Section from './Section';
+// import $ from 'jquery';
+// import axios from 'axios';
 
 
 const StyledContainer = styled.div`
@@ -15,66 +16,10 @@ const StyledContainer = styled.div`
 `;
 
 export default class Container extends React.Component {
-  state = {
-    error: null,
-    loading: false,
-    people: [],
-  }
-
-  setPeople = people => {
-    this.setState({
-      loading: false,
-      people,
-    });
-  }
-
-  setError = error => {
-    this.setState({
-      loading: false,
-      error: error.message,
-    });
-  }
-
-  doRequest = () => {
-    this.setState({ loading: true });
-    fetch('http://demo6368739.mockable.io/')
-      .then(res => res.json())
-      .then(this.setPeople)
-      .catch(this.setError);
-  }
-
-  componentDidMount() {
-    this.doRequest();
-  }
-
   render() {
-    if (this.state.loading) {
-      return <StyledContainer>Loading...</StyledContainer>;
-    }
-
-    if (this.state.error) {
-      return (
-        <StyledContainer>
-          This is horrible! {this.state.error}
-          <br />
-          <button onClick={this.doRequest}>Retry</button>
-        </StyledContainer>
-      );
-    }
-
     return (
       <StyledContainer>
-        {
-          this.state.people.length > 0 &&
-          this.state.people.map(person => (
-            <div>
-              name: {person.name}
-              <br />
-              age: {person.age}
-            </div>
-          ))
-        }
-        <button onClick={this.doRequest}>Fetch again</button>
+        HTTP/AJAX
       </StyledContainer>
     );
   }
